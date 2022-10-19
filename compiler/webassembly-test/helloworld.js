cd6f65dceba39956f9b489736ff5275a33fc6494
drewesch <wheels.esch@gmail.com>
2022-10-19 10:06:43 -0700

    Hello World in WebAssembly

---
+++ compiler/webassembly-test/helloworld.js
@@ -0,0 +1,21 @@
+var memory;
+
+fetch('./helloworld.wasm').then(response =>
+response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes, {
+env: {
+   jsprint: function jsprint(byteOffset) {
+      var s = '';
+      var a = new Uint8Array(memory.buffer);
+      for (var i = byteOffset; a[i]; i++) {
+      s += String.fromCharCode(a[i]);
+      }
+      document.write(s);
+   }
+}
+})
+).then(results => {
+instance = results.instance;
+memory = instance.exports.pagememory;
+instance.exports.helloworld();
+}).catch(console.error);
