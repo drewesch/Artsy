33a61b072a59aed762c27ab854ea9df26c51b58a
drewesch <wheels.esch@gmail.com>
2022-11-04 09:23:58 -0700

    Backup WASM V2

---
+++ compiler/examples/old-wasm-v2/webgen.h
@@ -0,0 +1,19 @@
+#ifndef WEBGEN_H 
+#define WEBGEN_H
+
+void initAssemblyFile();
+
+void generateModule();
+
+void generateDataVariables();
+
+void printNewLine();
+
+void generateTextSection();
+
+void completeFile();
+
+void generateWATcode();
+
+
+#endif
