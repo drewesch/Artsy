56426ccd1617a9e8b2e21ce5983841b6b6e7f82d
Asher Shores <71547146+asherShores5@users.noreply.github.com>
2022-12-02 19:00:32 -0700

    Add files via upload

---
+++ artsy-project.com/src/client/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="jumbotron text-center">
+    <h1>Online Code Editor</h1>
+    <p>Learning to Code Made Simple</p>
+  </div>
+);
+
+export default Home;
